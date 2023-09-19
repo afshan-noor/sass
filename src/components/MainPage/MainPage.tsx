cd142d174cdcaec8e38da56899d446efdfc0259e
afshan-noor <143619568+afshan-noor@users.noreply.github.com>
2023-09-19 01:25:01 -0700

    Main Page Changes

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -3,8 +3,18 @@ const HomePage=()=>{
     return(
         <> 
         <div className='mainpage'>
-      <h1>Welcome to home Page</h1>
-      <button>Click Here</button>
+        <div className='StyledFlex'>
+            <div>
+                <h1>Build the comunity your fans would love</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Iure quos repellat voluptatum corrupti maiores ducimus
+                      odit illum dolores modi ut aperiam tempore dicta saepe aliquid voluptas, 
+                    doloremque iste earum optio est accusamus omnis necessitatibus!</p>
+                    <button>Get Started for Free</button>
+            </div>
+
+           <img src="https://raw.githubusercontent.com/bradtraversy/huddle_styled_components/6033c3b282d305910a8b1424f80e2f31f629d653/public/images/illustration-mockups.svg" alt=""/>
+          </div>
       </div>
         </>
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,23 @@
-import '../Navbar/Navbar.css'
-const Navbar=()=>{
-    return(
-        <>
-       <div className="navbar">
-        <div className='links'>
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/profile">Profile</a>
-        <a href="/contact">Contact</a>
+import "../Navbar/Navbar.css";
+const Navbar = () => {
+  return (
+ 
+      <div className="navbar">
+        <div className="logo">
+          <img
+            src="https://raw.githubusercontent.com/bradtraversy/huddle_styled_components/6033c3b282d305910a8b1424f80e2f31f629d653/public/images/logo.svg"
+            alt="logo"
+          />
         </div>
-       </div>
-        </>
-
-    );
-}
+        <div className="links">
+          <a href="/home">Home</a>
+          <a href="/about">About</a>
+          <a href="/profile">Profile</a>
+          <a href="/contact">Contact</a>
+          <button>Try It Free</button>
+        </div>
+      </div>
+  
+  );
+};
 export default Navbar;

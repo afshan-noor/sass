186c1f7c6f8d9f24b844a2c9d90b6e6c92080bb1
afshan-noor <143619568+afshan-noor@users.noreply.github.com>
2023-09-19 00:00:09 -0700

    Changes

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import '../MainPage/MainPage.css'
+const HomePage=()=>{
+    return(
+        <> 
+        <div className='mainpage'>
+      <h1>Welcome to home Page</h1>
+      </div>
+        </>
+
+    );
+}
+export default HomePage;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,15 @@
+import '../Navbar/Navbar.css'
 const Navbar=()=>{
     return(
         <>
-        <ul>
-            <li><a>Home</a></li>
-            <li><a>About </a></li>
-            <li><a>Contact</a></li>
-        </ul>
+       <div className="navbar">
+        <div className='links'>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/profile">Profile</a>
+        <a href="/contact">Contact</a>
+        </div>
+       </div>
         </>
 
     );
